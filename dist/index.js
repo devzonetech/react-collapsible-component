0f79adea50267a2baf2ba98bb720897c0b9cfb4e
Zdravko Shishmanov <zdrsoft@gmail.com>
2018-02-15 22:47:58 +0200

    Module exports

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import CollapsibleComponent from './components_export';
-import CollapsibleHead from './components_export';
-import CollapsibleContent from './components_export';
+import CollapsibleComponent from './components_export.js';
+import CollapsibleHead from './components_export.js';
+import CollapsibleContent from './components_export.js';
 
 module.exports.CollapsibleComponent = CollapsibleComponent;
 module.exports.CollapsibleHead = CollapsibleHead;
